1dfd57e328f0756d4c6080a82b5789e51fe0c2cd
amirrezwa <amirrezwanoori@gmail.com>
2025-05-21 12:38:17 +0330

    first commit2

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-  base: '/traffic-light/',  // 👈 برای GitHub Pages
+  base: '/trafficLight/',  // 👈 برای GitHub Pages
   plugins: [react()],
 })
